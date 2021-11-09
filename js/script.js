3b8aab1951a26724ce1785d49e39a3d2d6b2bdfc
Giovanni <gio.a.98@gmail.com>
2021-11-09 14:49:55 +0100

    fine milestone 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
+    let num = i; //assegno a num il valore del contatore
+    if (i % 3 == 0) {
+        num = "Fizz"; //num diventa Fizz quando è multiplo di 3
+    }
+    if (i % 5 == 0) {
+        num = "Buzz"; //num diventa Buzz quando è multiplo di 5
+    }
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        num = "FizzBuzz"; //num diventa FizzBuzz quando è multiplo di 3 e 5
+    }
+    console.log(num)
 }
