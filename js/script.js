1972a4b393fb90eb09391f8298eb34b5c917c267
Giovanni <gio.a.98@gmail.com>
2021-11-09 16:49:28 +0100

    utilizzo classList

--- js/script.js
+++ js/script.js
@@ -2,19 +2,18 @@ let num, box;
 for (let i = 1; i <= 100; i++) {
     box = document.createElement("div"); //creo div box
     document.querySelector(".grid").append(box); //aggiungo un box nella griglia
-    if (i % 15 == 0) {
+    box.className = "box"; //assegno la classe .box al div
+        if (i % 15 == 0) {
         num = "FizzBuzz"; //num diventa FizzBuzz quando è multiplo di 3 e 5
-        box.className = "box box-pink"; //assegno la classe .box e .box-pink al div
+        box.classList.add("box-pink"); //aggiungo la classe .box-pink al box
     } else if (i % 3 == 0) {
         num = "Fizz"; //num diventa Fizz quando è multiplo di 3
-        box.className = "box box-green"; //assegno la classe .box e .box-green al div
+        box.classList.add("box-green"); //aggiungo la classe .box-green al box
     } else if (i % 5 == 0) {
         num = "Buzz"; //num diventa Buzz quando è multiplo di 5
-        box.className = "box box-yellow"; //assegno la classe .box e .box-yellow al div
+        box.classList.add("box-yellow"); //aggiungo la classe .box-yellow al box
     } else {
         num = i; //assegno a num il valore del contatore
-        box.className = "box"; //assegno la classe .box al div
     }
     box.innerHTML = num;
-    console.log(num)
 }
